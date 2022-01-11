57e0d3983b843efcea65ce8f7b793a345c20ee45
Vladislav <dudashvilivladislav@mail.ru>
2022-01-11 16:03:56 +0300

    add new request for db api/user/create

--- src/api/requests/auth.js
+++ src/api/requests/auth.js
@@ -5,13 +5,17 @@ export default {
     return axios.get("http://127.0.0.1:8000/api/user/detail/default/");
   },
 
-  create(username = 'default', password = "HLhm*Ppsv2q!AXG") {
+  create(username = "default", password = "HLhm*Ppsv2q!AXG") {
+    return axios.post("http://127.0.0.1:8000/api/user/token/", {
+      username,
+      password,
+    });
+  },
 
-    return axios.post('http://127.0.0.1:8000/api/user/token/',
-      {
-        username,
-        password
-      })
-
+  createUsersInDb(username, password) {
+    return axios.post("http://127.0.0.1:8000/api/user/create/", {
+      username,
+      password,
+    });
   },
 };
