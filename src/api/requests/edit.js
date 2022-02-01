a3421530432f51a99ab864a621bc2a005612604e
Viacheslav <vapoltavecs@gmail.com>
2022-02-01 10:35:12 +0400

    fix editing of images

--- src/api/requests/edit.js
+++ src/api/requests/edit.js
@@ -1,17 +1,26 @@
 import axios from "axios"
 
+
 export default {
     put(data, id) {
+
         const body = new FormData()
         for (let i in data) {
             if (Array.isArray(data[i])) {
-                data[i].forEach(el => body.append('images', el.file))
+                
+                data[i].forEach(element => {
+                    if (!element.file) {
+                        const filePath = element.substring(element.indexOf("post_images"), element.length)
+                        body.append("to_delete", filePath)
+                    }
+                });
             }
             else {
                 body.append(i, data[i])
             }
 
         }
+
         return axios.put(`http://92.255.109.134:3000/api/post/update/${id}/`, body, {
             headers: {
                 'Content-Type': 'multipart/form-data'
@@ -19,3 +28,24 @@ export default {
         })
     }
 }
+
+
+const func = () => {
+    new Proxy({
+        id: {
+            user: 1
+        }
+    }, {
+        get(target, name) {
+            if (typeof name === "object") {
+                func()
+            }
+            return target.name
+        },
+        set(target, name, value) {
+
+        }
+    })
+
+}
+
--- src/components/pages/edit/edit.jsx
+++ src/components/pages/edit/edit.jsx
@@ -7,24 +7,38 @@ import { Spinner } from "../../ui/spinner/spinner";
 import React from "react";
 import classes from "@styles/edit/edit.module.scss";
 import { useHistory } from "react-router-dom";
+import cross from "../../../../assets/icons/cross.svg";
 
 export const Edit = ({ match }) => {
   const { id } = match.params;
   const history = useHistory();
-  const [form, setForm] = useState();
+  const [form, setForm] = useState({
+    to_delete: [],
+    images: [],
+    caption: "",
+    name: "",
+    tags: "",
+    external_url: "",
+    category: "",
+  });
 
   useEffect(() => {
     const getPost = async () => {
       const post = (await requests.image.get(id)).data;
-      setForm(post);
+      setForm({...form, ...post});
     };
     getPost();
   }, []);
 
   const removeImg = (e) => {
-    if (e.target.classList.contains(classes.image__item)) {
+    if (e.target.classList.contains(classes.image__icon)) {
+      console.log(form.to_delete);
       setForm({
         ...form,
+        to_delete: [...form.to_delete, e.target.getAttribute("data-to-delete")],
+        images: form.images.filter(
+          (el) => form.images[e.target.getAttribute("data-id")].file != el.file
+        ),
       });
     }
   };
@@ -50,11 +64,17 @@ export const Edit = ({ match }) => {
       <div onClick={removeImg}>
         <Swiper {...settings} slidesPerView={3} className={classes.images}>
           {form.images.map((el, i) => (
-            <SwiperSlide key={i}>
+            <SwiperSlide key={i} className={classes.image}>
+              <img
+                src={cross}
+                alt="cross"
+                className={classes.image__icon}
+                data-to-delete={el.file}
+                data-id={i}
+              />
               <img
                 src={el?.file}
                 alt="image item"
-                data-id={i}
                 className={classes.image__item}
               />
             </SwiperSlide>
